3067c6c4c00ecc58266fc1d1700825d815b66047
erickubuntu <gestionecesaria@gmail.com>
2024-03-09 19:28:44 -0400

    9 de mar 2024 1er commit

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -8,122 +8,135 @@ export default function NavBar() {
   }, []);
 
   return (
-    <div className="container-fluid bg-dark ">
-      <nav className="navbar navbar-expand-sm  ">
-        <div className="container-fluid">
-          <li className="nav-item dropdown">
-            <a
-              className="navbar-brand dropdown-toggle text-white"
-              href="#"
-              role="button"
-              data-bs-toggle="dropdown"
+    <>
+      <div className="d-flex justify-content-around align-items-center  bg-white">
+        <div>Logo</div>
+        <div>Productos recien llegados</div>
+        <div className="d-grid">
+          <p>Atencion al cliente</p>
+          <p>(+58) 123 45 67</p>
+        </div>
+      </div>
+      <div className="container-fluid bg-dark ">
+        <nav className="navbar navbar-expand-sm  ">
+          <div className="container-fluid">
+            <li className="nav-item dropdown">
+              <a
+                className="navbar-brand dropdown-toggle text-white"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Categorias
+              </a>
+              <ul className="dropdown-menu">
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Fertilizantes
+                  </a>
+                </li>
+                <hr className="dropdown-divider" />
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Semillas
+                  </a>
+                </li>
+                <hr className="dropdown-divider" />
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Fungicidas
+                  </a>
+                </li>
+                <hr className="dropdown-divider" />
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Herbicidas
+                  </a>
+                </li>
+                <hr className="dropdown-divider" />
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Implementos Agricolas
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <button
+              className="navbar-toggler bg-white"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
               aria-expanded="false"
+              aria-label="Toggle navigation"
             >
-              Categorias
-            </a>
-            <ul className="dropdown-menu">
-              <li>
-                <a className="dropdown-item" href="#">
-                  Fertilizantes
-                </a>
-              </li>
-              <hr className="dropdown-divider" />
-              <li>
-                <a className="dropdown-item" href="#">
-                  Semillas
-                </a>
-              </li>
-              <hr className="dropdown-divider" />
-              <li>
-                <a className="dropdown-item" href="#">
-                  Fungicidas
-                </a>
-              </li>
-              <hr className="dropdown-divider" />
-              <li>
-                <a className="dropdown-item" href="#">
-                  Herbicidas
-                </a>
-              </li>
-              <hr className="dropdown-divider" />
-              <li>
-                <a className="dropdown-item" href="#">
-                  Implementos Agricolas
-                </a>
-              </li>
-            </ul>
-          </li>
-          <button
-            className="navbar-toggler bg-white"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto m-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a
-                  className="nav-link active text-white"
-                  aria-current="page"
-                  href="#"
-                >
-                  Inicio
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link text-white" href="#">
-                  Acerca
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link text-white" href="#">
-                  Contacto
-                </a>
-              </li>
-            </ul>
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav me-auto m-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a
+                    className="nav-link active text-white"
+                    aria-current="page"
+                    href="#"
+                  >
+                    Inicio
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link text-white" href="#">
+                    Acerca
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link text-white" href="#">
+                    Contacto
+                  </a>
+                </li>
+              </ul>
 
-            <div className="d-flex">
-              <div className="d-flex align-items-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 512 512"
-                  width={25}
-                  style={{ fill: "white" }}
-                >
-                  <path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z" />
-                </svg>
-                <span
-                  style={{ paddingBottom: "5px" }}
-                  className="badge border border-white rounded-circle text-white mx-2 pt-2"
-                >
-                  0
-                </span>
-              </div>
-              <div className="d-flex align-items-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 576 512"
-                  width={25}
-                  style={{ fill: "white" }}
-                >
-                  <path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z" />
-                </svg>
-                <span
-                  style={{ paddingBottom: "5px" }}
-                  className="badge border border-white rounded-circle text-white mx-2 pt-2"
-                >
-                  0
-                </span>
+              <div className="d-flex">
+                <div className="d-flex align-items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                    width={25}
+                    style={{ fill: "white" }}
+                  >
+                    <path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z" />
+                  </svg>
+                  <span
+                    style={{ paddingBottom: "5px" }}
+                    className="badge border border-white rounded-circle text-white mx-2 pt-2"
+                  >
+                    0
+                  </span>
+                </div>
+                <div className="d-flex align-items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 576 512"
+                    width={25}
+                    style={{ fill: "white" }}
+                  >
+                    <path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z" />
+                  </svg>
+                  <span
+                    style={{ paddingBottom: "5px" }}
+                    className="badge border border-white rounded-circle text-white mx-2 pt-2"
+                  >
+                    0
+                  </span>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </nav>
-    </div>
+        </nav>
+      </div>
+    </>
   );
 }
